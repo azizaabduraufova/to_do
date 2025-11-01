6d26a3b71b9795527e32862893a9d79694f641ed
aziza <aziza.abduraufova7@gmail.com>
2025-11-01 15:30:44 +0500

    Add .gitignore and configure PostgreSQL database settings

--- config/settings.py
+++ config/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'to_do_db',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
